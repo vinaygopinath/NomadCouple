92ba6693665c7c13813e3c07c9ad03dd11f41614
Vinay Gopinath <vinayg18@gmail.com>
2016-07-15 11:30:28 +0200

    [Enum] Visa and Person
    * Additional enum types in Visa
    * Visa <==> string conversion

--- src/app/person.enum.ts
+++ src/app/person.enum.ts
@@ -3,3 +3,16 @@ export enum Person {
   USER,
   PARTNER
 }
+
+export module Person {
+  export function toString(personType: Person) {
+    return Person[personType].toLowerCase();
+  }
+
+  export function parse(personType: string) {
+    if (!personType) {
+      throw new Error('Cannot parse empty string to Person enum');
+    }
+    return Person[personType.toUpperCase()];
+  }
+}
--- src/app/visa.enum.ts
+++ src/app/visa.enum.ts
@@ -1,6 +1,21 @@
 export enum Visa {
   REQUIRED,
   NOT_REQUIRED,
+  EVISA,
   ON_ARRIVAL,
+  REFUSED,
   UNKNOWN
+}
+
+export module Visa {
+  export function toString(visaType: Visa) {
+    return Visa[visaType].toLowerCase().replace(/_/g, '-');
+  }
+
+  export function parse(visaType: string) {
+    if (!visaType) {
+       throw new Error('Cannot parse empty string into a Visa enum');
+    }
+    return Visa[visaType.toUpperCase().replace(/-/g, '_')];
+  }
 }
