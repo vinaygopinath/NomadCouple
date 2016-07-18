9403c0219dce6ded5fdadf3bfb2e2006982ea95c
Vinay Gopinath <vinayg18@gmail.com>
2016-07-18 21:15:28 +0200

    [Component][Results] Animation on enter/card change
    * Web animations polyfill

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,11 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, trigger, state, transition, style, animate } from '@angular/core';
 import { CountryCardComponent } from '../country-card';
 @Component({
   moduleId: module.id,
   selector: 'nomad-results',
   templateUrl: 'results.component.html',
   styleUrls: ['results.component.css'],
-  directives: [CountryCardComponent]
+  directives: [CountryCardComponent],
+  animations: [
+  trigger('flyInOut', [
+    state('in', style({transform: 'translateX(0)'})),
+    transition('void => *', [
+      style({transform: 'translateX(-100%)'}),
+      animate('400ms cubic-bezier(0.23, 1, 0.32, 1)')
+    ]),
+    transition('* => void', [
+      animate(100, style({transform: 'translateX(200%)'}))
+    ])
+  ]),
+  trigger('zoomInOut', [
+    state('in', style({transform: 'rotateY(0deg)'})),
+    transition('void => *', [
+      style({transform: 'rotateY(180deg)'}),
+      animate('400ms cubic-bezier(0.23, 1, 0.32, 1)')
+    ]),
+    transition('* => void', [
+      animate(100, style({transform: 'rotateY(180deg)'}))
+    ])
+  ])
+]
 })
 export class ResultsComponent implements OnInit {
   @Input()
